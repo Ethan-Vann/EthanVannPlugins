b648d092b3508ec5797fa069580cd14d378008ed
Ethan Vann <Superevevev@gmail.com>
2023-03-01 09:40:41 -0500

    version 2.3

--- src/main/java/com/example/Packets/MousePackets.java
+++ src/main/java/com/example/Packets/MousePackets.java
@@ -7,13 +7,17 @@ import lombok.SneakyThrows;
 import net.runelite.api.Client;
 
 import javax.inject.Inject;
+import java.awt.event.KeyEvent;
 import java.math.BigInteger;
+import java.util.Random;
 
 public class MousePackets {
     @Inject
     Client client;
     @Inject
     PacketReflection packetReflection;
+    private static Random random = new Random();
+    private static long randomDelay =randomDelay();
 
     public static BigInteger modInverse(BigInteger val, int bits)
     {
@@ -52,9 +56,41 @@ public class MousePackets {
         packetReflection.sendPacket(PacketDef.EVENT_MOUSE_CLICK, mouseInfo, x, y);
         PacketReflection.mouseHandlerLastPressedTime.setAccessible(false);
         PacketReflection.clientMouseLastLastPressedTimeMillis.setAccessible(false);
+        if(checkIdleLogout()){
+            randomDelay = randomDelay();
+            pressKey();
+        }
     }
 
     public void queueClickPacket() {
         queueClickPacket(0, 0);
+    }
+    private boolean checkIdleLogout()
+    {
+        int idleClientTicks = client.getKeyboardIdleTicks();
+
+        if (client.getMouseIdleTicks() < idleClientTicks)
+        {
+            idleClientTicks = client.getMouseIdleTicks();
+        }
+
+        return idleClientTicks >= randomDelay;
+    }
+
+    private static long randomDelay()
+    {
+        return (long) clamp(
+                Math.round(random.nextGaussian() * 8000)
+        );
+    }
+    private static double clamp(double val)
+    {
+        return Math.max(1, Math.min(13000, val));
+    }
+    private void pressKey()
+    {
+        this.client.getCanvas().dispatchEvent(new KeyEvent(this.client.getCanvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_BACK_SPACE));
+        this.client.getCanvas().dispatchEvent(new KeyEvent(this.client.getCanvas(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, KeyEvent.VK_BACK_SPACE));
+        this.client.getCanvas().dispatchEvent(new KeyEvent(this.client.getCanvas(), KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_BACK_SPACE));
     }
 }
