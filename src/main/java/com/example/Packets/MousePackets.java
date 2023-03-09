2da608988ad703584e92c07006ca8d249b864181
Ethan Vann <Superevevev@gmail.com>
2023-03-09 11:31:19 -0500

    version 2.5

--- src/main/java/com/example/Packets/MousePackets.java
+++ src/main/java/com/example/Packets/MousePackets.java
@@ -46,7 +46,6 @@ public class MousePackets {
         long clientMs = Long.parseLong(ObfuscatedNames.ClientMouseHandlerGarbage)*(long)PacketReflection.clientMouseLastLastPressedTimeMillis.get(null);
         long mouseMs =System.currentTimeMillis();
         long deltaMs = mouseMs - clientMs;
-        System.out.println(deltaMs);
         if (deltaMs < 0) {
             deltaMs = 0L;
         }
