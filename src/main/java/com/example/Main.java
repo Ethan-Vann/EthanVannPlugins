b2250ca38f49e528cb2fe747ee5ffd7517a13102
bigdrizzle13 <alexjbecherer@gmail.com>
2023-03-16 08:08:46 -0700

    Add macOS support

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,10 +15,25 @@ public class Main
 {
 	public static void main(String args[]) throws IOException
 	{
+
+
 		ReadableByteChannel readableByteChannel = Channels.newChannel(new URL("https://github.com/Ethan-Vann/Installer/releases/download/1.0/RuneLiteHijack.jar").openStream());
-		FileOutputStream fileOutputStream = new FileOutputStream(System.getProperty("user.home")+"\\AppData\\Local\\RuneLite\\EthanVannInstaller.jar");
+		FileOutputStream fileOutputStream;
+
+		if(System.getProperty("os.name").contains("Mac OS X")) {
+			fileOutputStream = new FileOutputStream("/Applications/RuneLite.app/Contents/Resources/EthanVannInstaller.jar");
+		}else{
+			fileOutputStream = new FileOutputStream(System.getProperty("user.home")+"\\AppData\\Local\\RuneLite\\EthanVannInstaller.jar");
+		}
 		fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-		String file = System.getProperty("user.home")+"\\AppData\\Local\\RuneLite\\config.json";
+
+		String file;
+		if(System.getProperty("os.name").contains("Mac OS X")) {
+			file = "/Applications/RuneLite.app/Contents/Resources/config.json";
+		}else{
+			file = System.getProperty("user.home")+"\\AppData\\Local\\RuneLite\\config.json";
+		}
+
 		InputStream inputStream = new FileInputStream(file);
 		JSONTokener tokener = new JSONTokener(inputStream);
 		JSONObject object = new JSONObject(tokener);
