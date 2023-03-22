ae3fdd626763f49d23ec17aa6b1073b3a5b31371
Ethan Vann <Superevevev@gmail.com>
2023-03-22 13:23:19 -0400

    superglass changes

--- src/main/java/com/example/superglass/SuperGlassMakerPlugin.java
+++ src/main/java/com/example/superglass/SuperGlassMakerPlugin.java
@@ -1,15 +1,20 @@
 package com.example.superglass;
 
+import com.example.EthanApiPlugin.Bank;
+import com.example.EthanApiPlugin.BankInventory;
 import com.example.EthanApiPlugin.EthanApiPlugin;
+import com.example.EthanApiPlugin.NPCs;
+import com.example.EthanApiPlugin.TileObjects;
+import com.example.InteractionApi.NPCInteraction;
+import com.example.InteractionApi.TileObjectInteraction;
 import com.example.PacketUtilsPlugin;
 import com.example.Packets.MousePackets;
-import com.example.Packets.NPCPackets;
-import com.example.Packets.ObjectPackets;
 import com.example.Packets.WidgetPackets;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.ItemID;
 import net.runelite.api.NPC;
@@ -22,10 +27,9 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.plugins.PluginManager;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Comparator;
+import java.util.Optional;
 
 @PluginDescriptor(name = "Super Glass Maker", description = "", enabledByDefault = false, tags = {"ethan"})
 @Slf4j
@@ -36,16 +40,6 @@ public class SuperGlassMakerPlugin extends Plugin
 	public int timeout = 0;
 	@Inject
 	Client client;
-	@Inject
-	PluginManager pluginManager;
-	@Inject
-	WidgetPackets widgetPackets;
-	@Inject
-	MousePackets mousePackets;
-	@Inject
-	NPCPackets npcPackets;
-	@Inject
-	ObjectPackets objectPackets;
 	@Inject
 	EthanApiPlugin api;
 	@Inject
@@ -78,82 +72,91 @@ public class SuperGlassMakerPlugin extends Plugin
 			timeout--;
 			return;
 		}
-		NPC banker = client.getNpcs().stream().filter(npc -> npc.getName().toLowerCase().contains("bank")).sorted(Comparator.comparingInt(x -> x.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()))).findFirst().orElse(null);
-		TileObject bank = api.findObject("bank");
+		Optional<NPC> banker = NPCs.search().withAction("Bank").nearestToPlayer();
+		Optional<TileObject> bank = TileObjects.search().withAction("Bank").nearestToPlayer();
 		if (client.getWidget(WidgetInfo.BANK_CONTAINER) == null)
 		{
-			if (!interactBankOrBanker(banker, bank))
-			{
+			if(banker.isPresent()){
+				NPCInteraction.interact(banker.get(),"Bank");
+			}else if(bank.isPresent()){
+				TileObjectInteraction.interact(bank.get(),"Bank");
+			}else{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "couldn't find bank or banker", null);
 				api.stopPlugin(this);
 				return;
 			}
-			return;
 		}
-		Widget sand = api.getItem(ItemID.BUCKET_OF_SAND, WidgetInfo.BANK_ITEM_CONTAINER);
-		Widget glass = api.getItem(ItemID.MOLTEN_GLASS, WidgetInfo.BANK_INVENTORY_ITEMS_CONTAINER);
-		Widget astral = api.getItem(ItemID.ASTRAL_RUNE, WidgetInfo.BANK_INVENTORY_ITEMS_CONTAINER);
-		Widget secondary = api.getItem(config.secondary().getId(), WidgetInfo.BANK_ITEM_CONTAINER);
+		Optional<Widget> sand = Bank.search().withId(ItemID.BUCKET_OF_SAND).first();
+		Optional<Widget> glass = BankInventory.search().withId(ItemID.MOLTEN_GLASS).first();
+		Optional<Widget> astral = BankInventory.search().withId(ItemID.ASTRAL_RUNE).first();
+		Optional<Widget> secondary = Bank.search().withId(config.secondary().getId()).first();
 		Widget make_glass = client.getWidget(14286966);
-		if (sand == null || astral == null || secondary == null || make_glass == null)
+		if (sand.isEmpty()||astral.isEmpty()||secondary.isEmpty()|| make_glass == null)
 		{
+			if(sand.isEmpty()){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "no sand", null);
+			}
+			if(astral.isEmpty()){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "no astral", null);
+			}
+			if(secondary.isEmpty()){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "no secondary", null);
+			}
+			if(make_glass == null){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "spell was null", null);
+			}
 			api.stopPlugin(this);
 			return;
 		}
-		if (glass != null)
+		if (glass.isPresent())
 		{
-			mousePackets.queueClickPacket();
-			widgetPackets.queueWidgetAction(glass, "Deposit-All");
+			MousePackets.queueClickPacket();
+			WidgetPackets.queueWidgetAction(glass.get(), "Deposit-All");
 		}
-		mousePackets.queueClickPacket();
-		widgetPackets.queueWidgetAction(sand, "Withdraw-" + config.secondary().getSandAmount());
+		MousePackets.queueClickPacket();
+		WidgetPackets.queueWidgetAction(sand.get(), "Withdraw-" + config.secondary().getSandAmount());
 		boolean secondaryWithdrawn = handleSecondary();
-		if (!interactBankOrBanker(banker, bank)||!secondaryWithdrawn)
-		{
+		if(!secondaryWithdrawn){
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "no secondary stop plugin", null);
 			api.stopPlugin(this);
 			return;
 		}
-		mousePackets.queueClickPacket();
-		widgetPackets.queueWidgetAction(make_glass, "Cast");
+		if(banker.isPresent()){
+			NPCInteraction.interact(banker.get(),"Bank");
+		}else if(bank.isPresent()){
+			TileObjectInteraction.interact(bank.get(),"Bank");
+		}else{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "couldn't find bank or banker try 2", null);
+			api.stopPlugin(this);
+			return;
+		}
+		MousePackets.queueClickPacket();
+		WidgetPackets.queueWidgetAction(make_glass, "Cast");
 		timeout = 3;
 	}
 
 	public boolean handleSecondary()
 	{
-		Widget secondary = api.getItem(config.secondary().getId(), WidgetInfo.BANK_ITEM_CONTAINER);
-		if (secondary == null)
+		Optional<Widget> secondary = Bank.search().withId(config.secondary().getId()).first();
+		if (secondary.isEmpty())
 		{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "no secondary second try", null);
 			return false;
 		}
 		if (config.secondary() == Secondary.GIANT_SEAWEED)
 		{
-			mousePackets.queueClickPacket();
-			widgetPackets.queueWidgetAction(secondary, "Withdraw-1");
-			mousePackets.queueClickPacket();
-			widgetPackets.queueWidgetAction(secondary, "Withdraw-1");
-			mousePackets.queueClickPacket();
-			widgetPackets.queueWidgetAction(secondary, "Withdraw-1");
+			MousePackets.queueClickPacket();
+			WidgetPackets.queueWidgetAction(secondary.get(), "Withdraw-1");
+			MousePackets.queueClickPacket();
+			WidgetPackets.queueWidgetAction(secondary.get(), "Withdraw-1");
+			MousePackets.queueClickPacket();
+			WidgetPackets.queueWidgetAction(secondary.get(), "Withdraw-1");
 			return true;
 		}
-		mousePackets.queueClickPacket();
-		widgetPackets.queueWidgetAction(secondary, "Withdraw-"+config.secondary().getSandAmount());
+		MousePackets.queueClickPacket();
+		WidgetPackets.queueWidgetAction(secondary.get(), "Withdraw-"+config.secondary().getSandAmount());
 		return true;
 	}
 
-	public boolean interactBankOrBanker(NPC banker, TileObject bank)
-	{
-		if (banker != null)
-		{
-			mousePackets.queueClickPacket();
-			npcPackets.queueNPCAction(banker, "Bank");
-			return true;
-		}
-		if (bank != null)
-		{
-			mousePackets.queueClickPacket();
-			objectPackets.queueObjectAction(bank, false, "Bank");
-			return true;
-		}
-		return false;
-	}
 
 }
