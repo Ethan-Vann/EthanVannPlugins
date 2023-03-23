92a0e3b3a29b510bbdf2c2f7a7f885aaf20f3841
Ethan Vann <Superevevev@gmail.com>
2023-03-22 23:50:30 -0400

    changes : )

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,14 +1,10 @@
 package com.example;
 
-import com.example.AutoCalvarion.AutoCalvarion;
-import com.example.AutoSpindel.AutoSpindel;
-import com.example.AutoTele.AutoTele;
 import com.example.E3t4g.e3t4g;
 import com.example.EthanApiPlugin.EthanApiPlugin;
 import com.example.LavaRunecrafter.LavaRunecrafterPlugin;
 import com.example.NightmareHelper.NightmareHelperPlugin;
 import com.example.PrayerFlicker.EthanPrayerFlickerPlugin;
-import com.example.PvPKeysUITesting.PvPKeys;
 import com.example.UpkeepPlugin.UpkeepPlugin;
 import com.example.gauntletFlicker.gauntletFlicker;
 import com.example.harpoon2ticker.SwordFish2Tick;
@@ -20,11 +16,11 @@ public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(EthanApiPlugin.class,PacketUtilsPlugin.class, EthanPrayerFlickerPlugin.class,
+		ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, PacketUtilsPlugin.class, EthanPrayerFlickerPlugin.class,
 				gauntletFlicker.class,
 				SuperGlassMakerPlugin.class, UpkeepPlugin.class, LavaRunecrafterPlugin.class,
-				NightmareHelperPlugin.class, PvPKeys.class, AutoSpindel.class, SwordFish2Tick.class, AutoCalvarion.class
-				, e3t4g.class, AutoTele.class);
+				NightmareHelperPlugin.class, SwordFish2Tick.class
+				, e3t4g.class);
 		RuneLite.main(args);
 	}
 }
