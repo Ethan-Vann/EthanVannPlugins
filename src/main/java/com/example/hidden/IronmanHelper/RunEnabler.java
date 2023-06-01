4c7a7bd0bf26fbff1c808efb2577e70f0dc4540b
Ethan Vann <Superevevev@gmail.com>
2023-06-01 15:49:09 -0400

    make run enabler more readable

--- src/main/java/com/example/hidden/IronmanHelper/RunEnabler.java
+++ src/main/java/com/example/hidden/IronmanHelper/RunEnabler.java
@@ -10,15 +10,27 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 
 @PluginDescriptor(name = "RunEnabler", description = "", enabledByDefault = false, tags = {"ethan"})
-public class RunEnabler extends Plugin{
+public class RunEnabler extends Plugin {
     @Subscribe
     public void onGameTick(GameTick e) {
-        if(!EthanApiPlugin.loggedIn()){
+        if (!EthanApiPlugin.loggedIn()) {
             return;
         }
-        if(EthanApiPlugin.getClient().getVarpValue(173)==0&&EthanApiPlugin.getClient().getEnergy()>100){
-            MousePackets.queueClickPacket();
-            WidgetPackets.queueWidgetActionPacket(1,10485787,-1,-1);
+        if (runIsOff() && hasMoreThanZeroEnergy()) {
+            enableRun();
         }
+    }
+
+    boolean runIsOff() {
+        return EthanApiPlugin.getClient().getVarpValue(173) == 0;
+    }
+
+    boolean hasMoreThanZeroEnergy() {
+        return EthanApiPlugin.getClient().getEnergy() > 100;
+    }
+
+    void enableRun() {
+        MousePackets.queueClickPacket();
+        WidgetPackets.queueWidgetActionPacket(1, 10485787, -1, -1);
     }
 }
