43cfc2d2c649dd8c76018ada97299568a7621ba3
Hypsster <115741249+Hypsster@users.noreply.github.com>
2023-06-15 02:47:46 -0400

    Added to the "useItem" method. Allowing the method to accept sets as parameters now.
    I found this to be useful, when working with large groups of items / foods etc.

--- src/main/java/com/example/InteractionApi/InventoryInteraction.java
+++ src/main/java/com/example/InteractionApi/InventoryInteraction.java
@@ -6,6 +6,7 @@ import com.example.Packets.WidgetPackets;
 import net.runelite.api.widgets.Widget;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class InventoryInteraction {
@@ -18,8 +19,18 @@ public class InventoryInteraction {
         }).orElse(false);
     }
 
+
     public static boolean useItem(int id, String... actions) {
         return Inventory.search().withId(id).first().flatMap(item ->
+        {
+            MousePackets.queueClickPacket();
+            WidgetPackets.queueWidgetAction(item, actions);
+            return Optional.of(true);
+        }).orElse(false);
+    }
+
+    public static boolean useItem(Set<Integer> id, String... actions) {
+        return Inventory.search().withSet(id).first().flatMap(item ->
         {
             MousePackets.queueClickPacket();
             WidgetPackets.queueWidgetAction(item, actions);
