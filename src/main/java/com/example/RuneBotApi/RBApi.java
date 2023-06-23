cad572134ece16217a688e790732e6459d1e958d
xylphie <nanabread@protonmail.com>
2023-06-23 16:32:42 -0500

    Support arbitrary client thread invocations

---
+++ src/main/java/com/example/RuneBotApi/RBApi.java
@@ -0,0 +1,30 @@
+package com.example.RuneBotApi;
+
+import net.runelite.api.Client;
+import net.runelite.client.RuneLite;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.game.ItemManager;
+
+public class RBApi {
+
+
+    public static Client getClient()
+    {
+        return RuneLite.getInjector().getProvider(Client.class).get();
+    }
+
+    public static ItemManager getItemManager()
+    {
+        return RuneLite.getInjector().getProvider(ItemManager.class).get();
+    }
+
+    public static ClientThread getClientThread()
+    {
+        return RuneLite.getInjector().getProvider(ClientThread.class).get();
+    }
+
+    public static void runOnClientThread(Runnable r)
+    {
+        getClientThread().invoke(r);
+    }
+}
