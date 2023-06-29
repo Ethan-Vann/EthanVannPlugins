342d38ddbe74de1af73bee946a031415d06ebebd
xylphie <nanabread@protonmail.com>
2023-06-29 15:06:12 -0500

    Add csv config parser/matcher
    not case sensitive, returns true if the compared String is somewhere in the csv

--- src/main/java/com/example/RuneBotApi/RBApi.java
+++ src/main/java/com/example/RuneBotApi/RBApi.java
@@ -5,9 +5,13 @@ import net.runelite.client.RuneLite;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.game.WorldService;
+import net.runelite.client.util.Text;
+import net.runelite.client.util.WildcardMatcher;
 
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RBApi {
 
@@ -75,5 +79,36 @@ public class RBApi {
         return true;
     }
 
+
+    /**
+     * provide the config.itemList or config.npcList or w/e and
+     * use the resulting value for the configCSVParser. This helps
+     * improve time complexity if the user spells the item name
+     * correctly since having to search through every entity for every
+     * config entry runs in quadratic time which is no good
+     * @return configString -> HashSet<String>.forEach(str -> str.toLower().trim())
+     */
+    public static HashSet<String> configCSVToHashSet(String inputText)
+    {
+        return Text.fromCSV(inputText.toLowerCase()).stream()
+                .map(String::trim)
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    /**
+     * The nature of this being static (and potentially used concurrently)
+     * requires the instantiation of the HashSet to be done from within the calling method in
+     * order to see any performance benefit. See RBApi.configCSVToHashSet()
+     * @return true if item is in the config list
+     */
+    public static boolean configMatcher(HashSet<String> providedConfigEntries, String entityName)
+    {
+        if (providedConfigEntries.contains(entityName.toLowerCase())) return true;
+
+        return providedConfigEntries.stream()
+                .anyMatch(pattern ->
+                        WildcardMatcher.matches(pattern, entityName.toLowerCase())
+                );
+    }
 
 }
